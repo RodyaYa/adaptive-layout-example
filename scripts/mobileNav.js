1ecb690d715fc6667e34f681629c1feb34fd0aef
Rodion <rusyaloveee@gmail.com>
2021-05-18 19:58:20 +0300

    Mob. menu, upd. mob. media

--- scripts/mobileNav.js
+++ scripts/mobileNav.js
@@ -1,19 +1,19 @@
-const modal = document.querySelector("#mobile_header");
-const menu = document.querySelector("#mobile_nav");
-modal.addEventListener("click", menuHandler);
-console.log(menu);
+function _initialMobileMenu() {
+  const modal = document.querySelector("#mobile_header");
+  const menu = document.querySelector("#mobile_nav");
 
-function menuHandler(event) {
-  const action = event.target.dataset.menu;
-  console.log(action);
-  if (action === "close") {
-    console.log("close menu");
-    menu.classList.remove("opened");
-    document.body.style.overflow = "scroll";
-  }
-  if (action === "open") {
-    console.log("close menu");
-    menu.classList.add("opened");
-    document.body.style.overflow = "hidden";
+  modal.addEventListener("click", menuHandler);
+
+  function menuHandler(event) {
+    const action = event.target.dataset.menu;
+    if (action === "close") {
+      menu.classList.remove("opened");
+      document.body.style.overflow = "scroll";
+    } else if (action === "open") {
+      menu.classList.add("opened");
+      document.body.style.overflow = "hidden";
+    }
   }
 }
+
+_initialMobileMenu();
